9be97f1418382ea016d803f951d85ff814ec7eda
Onur Gültekin <onurgultekin@Onurs-MacBook-Pro.local>
2021-11-11 20:41:25 +0300

    create models & controllers and migrations

---
+++ app/Http/Controllers/API/DeviceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Device;
+
+class DeviceController extends Controller
+{
+    public function index() {
+        // get all devices may be.
+    }
+}
---
+++ database/migrations/2021_11_11_173105_create_devices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDevicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->bigInteger("id");
+            $table->string("uuid");
+            $table->string("language", 3);
+            $table->string("os", 10);
+            $table->string("client_token");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('devices');
+    }
+}
